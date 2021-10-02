b05aa5354458b032073647e9b44fdd96b6884247
Chris Zhu <czhuxj@gmail.com>
2021-10-02 10:52:47 +0800

    Clean up os

--- src/cst/unx/os/socket.cpp
+++ src/cst/unx/os/socket.cpp
@@ -5,90 +5,6 @@ namespace cst {
 namespace unx {
 namespace os {
 
-int Socket(int domain, int type, int protocol, std::error_code* ec)
-{
-    int fd = ::socket(domain, type, protocol);
-    if (fd == -1) {
-        if (ec)
-            *ec = Make_os_error_code(errno);
-        else
-            Throw_os_error(errno, "::socket()");
-    }
-    return fd;
-}
-
-int Connect(int sockfd, const sockaddr* addr, socklen_t addrlen, std::error_code* ec)
-{
-    int rc = ::connect(sockfd, addr, addrlen);
-    if (rc == -1) {
-        if (ec)
-            *ec = Make_os_error_code(errno);
-        else
-            Throw_os_error(errno, "::connect()");
-    }
-    return rc;
-}
-
-int Bind(int sockfd, const sockaddr* addr, socklen_t addrlen, std::error_code* ec)
-{
-    int rc = ::bind(sockfd, addr, addrlen);
-    if (rc == -1) {
-        if (ec)
-            *ec = Make_os_error_code(errno);
-        else
-            Throw_os_error(errno, "::bind()");
-    }
-    return rc;
-}
-
-int Listen(int sockfd, int backlog, std::error_code* ec)
-{
-    int rc = ::listen(sockfd, backlog);
-    if (rc == -1) {
-        if (ec)
-            *ec = Make_os_error_code(errno);
-        else
-            Throw_os_error(errno, "::listen()");
-    }
-    return rc;
-}
-
-int Accept(int sockfd, sockaddr* addr, socklen_t* addrlen, std::error_code* ec)
-{
-    int connfd = ::accept(sockfd, addr, addrlen);
-    if (connfd == -1) {
-        if (ec)
-            *ec = Make_os_error_code(errno);
-        else
-            Throw_os_error(errno, "::accept()");
-    }
-    return connfd;
-}
-
-int Accept4(int sockfd, sockaddr* addr, socklen_t* addrlen, int flags, std::error_code* ec)
-{
-    int connfd = ::accept4(sockfd, addr, addrlen, flags);
-    if (connfd == -1) {
-        if (ec)
-            *ec = Make_os_error_code(errno);
-        else
-            Throw_os_error(errno, "::accept4()");
-    }
-    return connfd;
-}
-
-ssize_t Recv(int sockfd, void* buf, size_t len, int flags, std::error_code* ec)
-{
-    ssize_t n = ::recv(sockfd, buf, len, flags);
-    if (n == -1) {
-        if (ec)
-            *ec = Make_os_error_code(errno);
-        else
-            Throw_os_error(errno, "::recv()");
-    }
-    return n;
-}
-
 ssize_t Recvn(int sockfd, void* buf, size_t len, int flags, std::error_code* ec)
 {
     size_t nrecv = 0;
@@ -114,18 +30,6 @@ ssize_t Recvn(int sockfd, void* buf, size_t len, int flags, std::error_code* ec)
     return nrecv;
 }
 
-ssize_t Send(int sockfd, const void* buf, size_t len, int flags, std::error_code* ec)
-{
-    ssize_t n = ::send(sockfd, buf, len, flags);
-    if (n == -1) {
-        if (ec)
-            *ec = Make_os_error_code(errno);
-        else
-            Throw_os_error(errno, "::send()");
-    }
-    return n;
-}
-
 ssize_t Sendn(int sockfd, const void* buf, size_t len, int flags, std::error_code* ec)
 {
     size_t nsent = 0;
@@ -149,42 +53,6 @@ ssize_t Sendn(int sockfd, const void* buf, size_t len, int flags, std::error_cod
     }
 
     return nsent;
-}
-
-int Getsockopt(int sockfd, int level, int opt, void* val, socklen_t* len, std::error_code* ec)
-{
-    int rc = ::getsockopt(sockfd, level, opt, val, len);
-    if (rc == -1) {
-        if (ec)
-            *ec = Make_os_error_code(errno);
-        else
-            Throw_os_error(errno, "::getsockopt()");
-    }
-    return rc;
-}
-
-int Setsockopt(int sockfd, int level, int opt, const void* val, socklen_t len, std::error_code* ec)
-{
-    int rc = ::setsockopt(sockfd, level, opt, val, len);
-    if (rc == -1) {
-        if (ec)
-            *ec = Make_os_error_code(errno);
-        else
-            Throw_os_error(errno, "::setsockopt()");
-    }
-    return rc;
-}
-
-int Shutdown(int sockfd, int how, std::error_code* ec)
-{
-    int rc = ::shutdown(sockfd, how);
-    if (rc == -1) {
-        if (ec)
-            *ec = Make_os_error_code(errno);
-        else
-            Throw_os_error(errno, "::shutdown()");
-    }
-    return rc;
 }
 
 }
--- src/cst/unx/os/unistd.cpp
+++ src/cst/unx/os/unistd.cpp
@@ -5,54 +5,6 @@ namespace cst {
 namespace unx {
 namespace os {
 
-int Truncate(const char* path, off_t len, std::error_code* ec)
-{
-    int r = ::truncate(path, len);
-    if (r == -1) {
-        if (ec)
-            *ec = Make_os_error_code(errno);
-        else
-            Throw_os_error(errno, "::truncate()");
-    }
-    return r;
-}
-
-int Ftruncate(int fd, off_t len, std::error_code* ec)
-{
-    int r = ::ftruncate(fd, len);
-    if (r == -1) {
-        if (ec)
-            *ec = Make_os_error_code(errno);
-        else
-            Throw_os_error(errno, "::ftruncate()");
-    }
-    return r;
-}
-
-off_t Lseek(int fd, off_t off, int whence, std::error_code* ec)
-{
-    off_t ofs = ::lseek(fd, off, whence);
-    if (ofs == -1) {
-        if (ec)
-            *ec = Make_os_error_code(errno);
-        else
-            Throw_os_error(errno, "::lseek()");
-    }
-    return ofs;
-}
-
-ssize_t Read(int fd, void* buf, size_t len, std::error_code* ec)
-{
-    ssize_t n = ::read(fd, buf, len);
-    if (n == -1) {
-        if (ec)
-            *ec = Make_os_error_code(errno);
-        else
-            Throw_os_error(errno, "::read()");
-    }
-    return n;
-}
-
 ssize_t Readn(int fd, void* buf, size_t len, std::error_code* ec)
 {
     size_t nread = 0;
@@ -78,18 +30,6 @@ ssize_t Readn(int fd, void* buf, size_t len, std::error_code* ec)
     return nread;
 }
 
-ssize_t Write(int fd, const void* buf, size_t len, std::error_code* ec)
-{
-    ssize_t n = ::write(fd, buf, len);
-    if (n == -1) {
-        if (ec)
-            *ec = Make_os_error_code(errno);
-        else
-            Throw_os_error(errno, "::write()");
-    }
-    return n;
-}
-
 ssize_t Writen(int fd, const void* buf, size_t len, std::error_code* ec)
 {
     size_t nwritten = 0;
@@ -113,66 +53,6 @@ ssize_t Writen(int fd, const void* buf, size_t len, std::error_code* ec)
     }
 
     return nwritten;
-}
-
-int Close(int fd, std::error_code* ec)
-{
-    int r = ::close(fd);
-    if (r == -1) {
-        if (ec)
-            *ec = Make_os_error_code(errno);
-        else
-            Throw_os_error(errno, "::close()");
-    }
-    return r;
-}
-
-int Dup(int oldfd, std::error_code* ec)
-{
-    int newfd = ::dup(oldfd);
-    if (newfd == -1) {
-        if (ec)
-            *ec = Make_os_error_code(errno);
-        else
-            Throw_os_error(errno, "::dup()");
-    }
-    return newfd;
-}
-
-int Dup2(int oldfd, int newfd, std::error_code* ec)
-{
-    int fd = ::dup2(oldfd, newfd);
-    if (fd == -1) {
-        if (ec)
-            *ec = Make_os_error_code(errno);
-        else
-            Throw_os_error(errno, "::dup2()");
-    }
-    return fd;
-}
-
-int Dup3(int oldfd, int newfd, int flags, std::error_code* ec)
-{
-    int fd = ::dup3(oldfd, newfd, flags);
-    if (fd == -1) {
-        if (ec)
-            *ec = Make_os_error_code(errno);
-        else
-            Throw_os_error(errno, "::dup3()");
-    }
-    return fd;
-}
-
-pid_t Fork(std::error_code* ec)
-{
-    pid_t pid = ::fork();
-    if (pid == -1) {
-        if (ec)
-            *ec = Make_os_error_code(errno);
-        else
-            Throw_os_error(errno, "::fork()");
-    }
-    return pid;
 }
 
 }
